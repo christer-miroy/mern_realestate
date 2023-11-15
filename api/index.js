a0592b8621907ce3106f516f8c49a3c7710c3854
Christer Miroy <chrstrzm.mry@gmail.com>
2023-11-15 23:06:17 +0800

    create error middleware and handle possible errors

--- api/index.js
+++ api/index.js
@@ -26,3 +26,14 @@ app.listen(port, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+// error middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
