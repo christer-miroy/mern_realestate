155212831910731696fd1a7b5a68e33d650fd521
Christer Miroy <chrstrzm.mry@gmail.com>
2023-11-15 22:55:19 +0800

    signup api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -16,11 +17,12 @@ mongoose
 ;
 
 const app = express();
+app.use(express.json());
 
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
 
-app.use(express.json());
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
