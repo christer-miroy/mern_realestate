93c6fe57096ffce31691fbd18d08d22cbe1f3627
Christer Miroy <chrstrzm.mry@gmail.com>
2023-11-17 17:19:41 +0800

    create lising api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -29,6 +30,7 @@ app.listen(port, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 // error middleware
 app.use((err, req, res, next) => {
