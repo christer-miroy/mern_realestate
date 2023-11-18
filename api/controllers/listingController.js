1e178469e73da9a4fcba82737ae21b8b72dddde9
Christer Miroy <chrstrzm.mry@gmail.com>
2023-11-19 01:24:12 +0800

    update listing api done via refact

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -1,4 +1,7 @@
+
+import mongoose from "mongoose";
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
     try {
@@ -28,3 +31,28 @@ export const deleteListing = async (req, res, next) => {
         next(error)
     }
 }
+
+export const updateListing = async (req, res, next) => {
+    try {
+        const listingId = req.params.id;
+
+        if (!mongoose.Types.ObjectId.isValid(listingId)) {
+            throw errorHandler(400, "Invalid listing ID");
+        }
+
+        const listingByUser = await Listing.findById(listingId);
+        
+        if (req.user.id !== listingByUser.userRef) {
+            throw errorHandler(401, "You can only update your own listings");
+        }
+        
+        const updatedListing = await Listing.findByIdAndUpdate(listingId, {
+            $set: req.body
+        }, { new: true });
+        
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+}
+
