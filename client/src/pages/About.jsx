c4595c7f5fbbc1ddf51d5fbbf607975ec06cc2e5
Christer Miroy <chrstrzm.mry@gmail.com>
2023-11-25 03:32:06 +0800

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,17 @@
 const About = () => {
   return (
-    <div>About</div>
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4">About MERN Estate</h1>
+      <p className="mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, placeat voluptatem. Tenetur aliquid illo omnis ad mollitia aut officiis sapiente laudantium voluptate dolores id voluptates adipisci, nesciunt incidunt aspernatur similique?
+      </p>
+      <p className="mb-4">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi consequuntur excepturi rerum itaque harum ipsam est, odit vel vitae sed! Vel pariatur eveniet facilis quisquam atque consequatur debitis molestiae similique.
+      </p>
+      <p className="mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, dignissimos similique sint nostrum nisi adipisci culpa. Deleniti optio enim quo architecto eius quaerat qui aut, quis est facilis adipisci officia.
+      </p>
+    </div>
   )
 }
 export default About
